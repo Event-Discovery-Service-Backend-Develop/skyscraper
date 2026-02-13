11db0d388df1e05e3979fb6f20b571938a7289aa
gulsezim <adilgulsezim04@icloud.com>
2026-02-13 20:21:28 +0500

    feat: add JWT protection and pagination to works endpoint

--- harvester/views.py
+++ harvester/views.py
@@ -1,25 +1,31 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
+
 from .models import Work
 from .serializers import WorkSerializer
+from .pagination import StandardResultsSetPagination
 
 
 class WorkViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = WorkSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
-        qs = Work.objects.all()
+        qs = Work.objects.all().order_by("-publication_year", "-id")
+
         year = self.request.query_params.get("year")
         if year:
             try:
                 qs = qs.filter(publication_year=int(year))
             except ValueError:
                 pass
+
         search = self.request.query_params.get("search")
         if search:
             qs = qs.filter(
                 Q(title__icontains=search) | Q(keywords__icontains=search)
             )
-        return qs.order_by("-created_at")
+
+        return qs
